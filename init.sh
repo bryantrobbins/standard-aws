c43670ad64d96136ff8dafba4402121a50949bf0
Bryan Robbins <bryan.robbins@finra.org>
2016-02-13 00:44:31 -0500

    Reduce to single init script from cloud.json

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -v
+
+echo "Updating packages"
+yum update -y
+
+echo "Installing packages"
+yum install -y rubygems git puppet3
+
+echo "Installing r10k"
+gem install r10k
+
+echo "Running r10k"
+pushd /root/bootstrap/instances/build
+r10k puppetfile install -v | tee /var/log/puppet/r10k.log
+popd
+
+echo "Running Puppet"
+pushd /root/bootstrap/instances/build
+puppet apply -l /var/log/puppet/site.log site.pp
+popd
+
+echo "Done with INIT."
