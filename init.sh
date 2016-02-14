4993390e9ccf6fbf977babfe75154c0b107fffc1
Bryan Robbins <bryan.robbins@finra.org>
2016-02-13 22:57:11 -0500

    Refactoring puppet mgmt

--- init.sh
+++ init.sh
@@ -1,27 +1,25 @@
 #!/bin/bash -v
 
 host=$1
-hostdir="/root/bootstrap/hosts/${host}"
+folder=$2
 
 echo "Installing on host ${host}"
 
 echo "Updating packages"
 yum update -y
 
-echo "Installing packages"
+echo "Installing yum packages"
 yum install -y rubygems git puppet3
 
-echo "Installing r10k"
-gem install r10k
+echo "Installing rubygems"
+gem install r10k hiera-eyaml hiera-eyaml-kms
+
+echo "Fetching latest Puppet configurations"
 
 echo "Running r10k"
-pushd ${hostdir}
 /usr/local/bin/r10k puppetfile install -v debug2 &> /var/log/puppet/r10k.log
-popd
 
 echo "Running Puppet"
-pushd ${hostdir}
-/usr/bin/puppet apply -l /var/log/puppet/site.log site.pp
-popd
+/usr/bin/puppet apply -l /var/log/puppet/site.log --hiera_config hiera.yaml site.pp
 
 echo "Done with INIT."
