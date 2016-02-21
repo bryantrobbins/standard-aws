e01f661d53152bc31408496ab3dd4db5563a22f7
Bryan Robbins <bryan.robbins@finra.org>
2016-02-20 21:25:55 -0500

    Separate update script

--- init/init.sh
+++ init/init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -v
 
-host=$1
-folder=$2
-
-echo "Installing on host ${host}"
-
 echo "Updating packages"
 yum update -y
 
@@ -14,12 +9,6 @@ yum install -y rubygems git puppet3
 echo "Installing rubygems"
 gem install r10k hiera-eyaml hiera-eyaml-kms
 
-echo "Running r10k"
-pushd /root/init
-/usr/local/bin/r10k puppetfile install -v debug &> r10k.log
-
-echo "Running Puppet"
-puppet apply site.pp --hiera_config hiera.yaml --modulepath=modules:site &> puppet.log
-popd
+./update.sh
 
 echo "Done with INIT."
