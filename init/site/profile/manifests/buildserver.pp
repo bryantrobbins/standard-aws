87651427bb5260f6803cfe568d93e568280a86f3
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-12 14:37:11 -0500

    Add gradle, consul, nginx

--- init/site/profile/manifests/buildserver.pp
+++ init/site/profile/manifests/buildserver.pp
@@ -1,3 +1,14 @@
 class profile::buildserver {
   include superbuilds
+  class { 'nginx': }
+  class { '::consul':
+    config_hash => {
+      'bootstrap_expect' => 1,
+      'data_dir'         => '/opt/consul',
+      'datacenter'       => 'primary',
+      'log_level'        => 'INFO',
+      'node_name'        => 'server',
+      'server'           => true,
+    }
+  }
 }
