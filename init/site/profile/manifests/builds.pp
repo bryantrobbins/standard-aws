4b712b88c86b4a29e33d3300353dba5ab4bfcf50
Bryan Robbins <bryantrobbins@gmail.com>
2016-03-28 00:12:52 -0400

    Add docker hotfix (seriously docker ... failboat); add consul service reg for jenkins

--- init/site/profile/manifests/builds.pp
+++ init/site/profile/manifests/builds.pp
@@ -1,3 +1,15 @@
 class profile::builds {
   include superbuilds
+
+  ::consul::service { 'jenkins':
+    checks  => [
+      {
+        script   => '/usr/bin/curl localhost:8080',
+        interval => '10s'
+      }
+    ],
+    port    => 8080,
+    tags    => ['build']
+  }
+
 }
